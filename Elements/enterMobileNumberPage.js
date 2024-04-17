35bcbd3144a7070492af7b6b27c5fa4b28e182b0
AmitG7781 <amit@paywithatoa.co.uk>
2024-04-17 15:26:34 +0530

    added test for mobile and email fiels validations

--- Elements/enterMobileNumberPage.js
+++
@@ -1,7 +0,0 @@
-output.enterMobileNumberPage = {
-    countryCode: '  +44',
-    mobileNumberTextField: 'Email or mobile number',
-    continueButton: 'Sign up',
-
-    employeeMobileError: 'This phone number is already registered to an Employee account. Please use a different phone number or reach out to us at hello@paywithatoa.co.uk',
-}
---
+++ Elements/signupPage.js
@@ -0,0 +1,23 @@
+output.signupPage = {
+    
+    signupPageTitle: 'Sign up',
+    loginPageTitle: 'Log in',
+
+    mobileOrEmailTextField: 'Email or mobile number',
+
+    signupButton: 'Sign up',
+    loginButton: 'Log in',
+
+    googleSignupButton: 'Sign up with Google',
+    googleLoginButton: 'Sign in with Google',
+
+
+    termsOfUseLink: 'Terms of Use',
+    termsOfServiceLink: 'Terms of Service',
+    privacyPolicyLink: 'Privacy Policy',
+    
+    loginLink: 'LOG IN',
+    signupLink: 'SIGN UP',
+
+    employeeMobileError: 'This phone number is already registered to an Employee account. Please use a different phone number or reach out to us at hello@paywithatoa.co.uk',
+}
