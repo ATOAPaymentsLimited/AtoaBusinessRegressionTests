357bd3f509180bd8139e189f90ad4b523589d28a
Yash Tiwari <yasht.2701@gmail.com>
2024-05-13 12:14:13 +0530

    added test for api access

---
+++ Elements/aipAccessPage.js
@@ -0,0 +1,25 @@
+output.apiAccessPage = {
+
+    enableApiAccessButton: 'Enable API Access',
+    sandboxToggle: 'Sandbox Custom Switch',
+    prodToggle: 'Production Custom Switch',
+
+    redirectUrlTextField: 'Redirect URL(Optional) TextFormField',
+    enableWithoutUrlButton: 'Enable without Redirect URL',
+    enableButton: 'Enable',
+
+    invalidUrlError: 'Redirect URL is invalid',
+
+    enabledSuccessfullyText: 'Enabled Successfully',
+    copyButton: 'Copy',
+    shareButton: 'Share',
+    closeButton: 'Close',
+
+    accessIdField: 'Access ID TextFormField',
+    accessSecretField: 'Access Secret TextFormField',
+    redirectUrlField: 'Redirect URL TextFormField',
+
+    disableButton: 'Disable',
+
+
+}
