b40309538b3dc778d8d4b28af1bfe771f340965c
Yash Tiwari <yasht.2701@gmail.com>
2024-05-14 19:46:33 +0530

    added test for employees profile verification

--- Elements/employeeProfilePage.js
+++ Elements/employeeProfilePage.js
@@ -2,8 +2,12 @@ output.employeeProfilePage = {
     profilePageTitle: 'Profile',
     addPhoto: 'Add Photo',
     changePhoto: 'Change Photo',
+    firstNameField: 'First Name* TextFormField',
     firstName: 'First Name*',
+    lastNameField: 'Last Name* TextFormField',
     lastName: 'Last Name*',
+    emailField: 'Email Address* TextFormField',
     email: 'Email Address*',
-    mobile: 'Mobile Number',
+    mobileField: 'Mobile Number (Optional) TextFormField',
+    mobile: 'Mobile Number (Optional)',
 }
