e1c6fcda309d966f62d4909b421ed6da97e6ae1c
Yash Tiwari <yasht.2701@gmail.com>
2024-05-17 13:14:28 +0530

    added test for notification tray page verification

--- Elements/merchantHomePage.js
+++ Elements/merchantHomePage.js
@@ -16,10 +16,6 @@ output.merchantHomePage = {
     rewardsButton: 'Rewards',
     referAndEarnButton: 'Refer & Earn',
     orderQRButton: 'Order QR',
-    showQRButton: 'Show QR',
-    tipsButton: 'Tipping',
-    rewardsButton: 'Rewards',
-    inviteButton: 'Invite',
     offerBanner: 'Offer Banner',
     copyrightText: '©2023 Atoa Payments Ltd. All logos and trademarks are property of their respective owners. Atoa Payments Ltd operates under a license issued to Yapily Connect Ltd (#827001) which are regulated by the UK Financial Conduct Authority under the Payment Services Regulations 2017 as an Authorized Payment Institution to provide account information services and payment initiation services.',
     
---
+++ Elements/notificationTrayPage.js
@@ -0,0 +1,8 @@
+output.notificationTrayPage = 
+{
+    notificationTile: 'Notification',
+    paymentFailedTitle: 'Payment Failed',
+    paymentPendingTitle: 'Payment Pending',
+    atoaTitle: 'Atoa Business',
+    refundCompleteTitle: 'Refund Complete!',
+}
