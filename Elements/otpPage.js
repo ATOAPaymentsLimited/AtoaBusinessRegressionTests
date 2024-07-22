5405b6f3f0ed3ebee5a417764d256d21b3ab40b4
Yash Tiwari <yasht.2701@gmail.com>
2024-07-22 19:30:38 +0530

    added test for OTP field verification

--- Elements/otpPage.js
+++ Elements/otpPage.js
@@ -2,6 +2,9 @@ output.otpPage = {
     otpPageTitle: 'Enter Code',
     enterCodeText: 'Enter the secret code sent to your phone at',
     countryCodeUK: '+44',
+
+    enterCodeText: 'To keep your account secure, please enter the 6-digit code sent to',
+    andText: 'and',
     
     otpTextField: 'OTP input',
 
@@ -14,5 +17,6 @@ output.otpPage = {
     invalidOtpError3: 'Incorrect code used. 3 attempts remaining',
     invalidOtpError4: 'Incorrect code used. 2 attempts remaining',
     invalidOtpError5: 'Incorrect code used. 1 attempt remaining',
-    invalidOtpError6: 'Maximum number of attempts reached. Please generate a new OTP',
+    invalidOtpError6: 'Incorrect code used. 0 attempts remaining',
+    invalidOtpError7: 'Maximum number of attempts reached. Please generate a new OTP',
 }
