3a4b8b9051eae6df003b5c43681529d518468a37
AmitG7781 <amit@paywithatoa.co.uk>
2024-08-07 14:59:44 +0530

    added method to verify the permissions in employee details page

--- Elements/employeeDetailsPage.js
+++ Elements/employeeDetailsPage.js
@@ -44,4 +44,22 @@ output.employeeDetailsPage = {
     employeeEmailExistingErrorToast: 'Employee with this email already exists',
 
     addOnEmployeeLimitErrorToast: `You've hit the maximum employee limit of 1 with your current plan. Contact sales and get your plan customized`,
+
+    viewOwnPaymentsCheckbox: 'Can view only their own payments and export Check Box',
+    viewOwnPaymentsText: 'Can view only their own payments and export',
+
+    viewAllPaymentsCheckbox: 'Can view all payments and export Check Box',
+    viewAllPaymentsText: 'Can view all payments and export',
+
+    viewEmployeesCheckbox: 'Can view employees Check Box',
+    viewEmployeesText: 'Can view employees',
+
+    manageEmployeesCheckbox: 'Can view, add and remove employees Check Box',
+    manageEmployeesText: 'Can view, add and remove employees',
+
+    bulkPaymentsCheckbox: 'Bulk payments Check Box',
+    bulkPaymentsText: 'Bulk payments',
+
+    billingsCheckbox: 'Can view and download billing invoices Check Box',
+    billingsText: 'Can view and download billing invoices',
 }
